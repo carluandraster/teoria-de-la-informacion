e21045393bec02e3fd59368e1fd0bf94f50d94c4
carluandraster <andres.efstratiadis@gmail.com>
2025-08-31 15:17:07 -0300

    Update ejercicio8.py

--- TP 2/ejercicio8.py
+++ TP 2/ejercicio8.py
@@ -1,7 +1,6 @@
 import Utils
 
 # Calcula la entropía de una fuente binaria de memoria nula
+# Precondición: omega debe estar en el rango [0, 1]
 def entropiaBinaria(omega: float) -> float:
-    if omega < 0 or omega > 1:
-        raise ValueError("La probabilidad debe estar en el rango [0, 1]")
     return Utils.entropia([omega, 1 - omega])
