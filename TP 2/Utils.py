bfa16f6c3872430b82e56ec10af13d0644d6857a
carluandraster <andres.efstratiadis@gmail.com>
2025-09-05 17:50:38 -0300

    Update Utils.py

--- TP 2/Utils.py
+++ TP 2/Utils.py
@@ -1,30 +1,45 @@
 import math
-from Matriz import Matriz
-import MatrizFactory as mf
 
-# Función para calcular la cantidad de información
-# p: probabilidad del evento
-# r: base del logaritmo (default 2)
-# retorna la cantidad de información en bits (si r=2)
 def cantidadInformacion(p: float, r=2) -> float:
+    """
+    Calcula la cantidad de información de un evento dado su probabilidad.
+    
+    Parámetros:
+        p (float): La probabilidad del evento (0 < p <= 1).
+        r (int): La base del logaritmo (default 2).
+    Retorna:
+        float: La cantidad de información en bits.
+    """
     if p <= 0 or p > 1:
         resultado = 0
     else:
         resultado = math.log(1/p, r)
     return resultado
 
-# Funcion para calcular entropia
-# probabilidades: lista de probabilidades de los eventos
-# r: base del logaritmo (default 2)
-# retorna la entropia en bits (si r=2)
 def entropia(probabilidades: list, r=2) -> float:
+    """
+    Calcula la entropía de una fuente de información dada una lista de probabilidades.
+    
+    Parámetros:
+        probabilidades (list): Lista de probabilidades de los eventos.
+        r (int): Base del logaritmo (default 2).
+    Retorna:
+        float: La entropía en la unidad que corresponda (por defecto, en bits).
+    """
     H = 0
     for p in probabilidades:
         H += p * cantidadInformacion(p, r)
     return H
 
-# Funcion que devuelve alfabeto con sus probabilidades
 def getAlfaProbabilidades(texto: str):
+    """
+    Dada una cadena de caracteres, devuelve una lista con su alfabeto y una lista con sus probabilidades.
+    
+    Parámetros:
+        texto (str): La cadena de caracteres.
+    Retorna:
+        tuple: Una tupla que contiene una lista con el alfabeto y una lista con sus probabilidades.
+    """
     alfabeto = []
     probabilidades = []
     longitud = len(texto)
