79e5a8a5f256a050d7c6705cd0617d4061033280
carluandraster <andres.efstratiadis@gmail.com>
2025-09-04 22:31:39 -0300

    Update Utils.py

--- TP 2/Utils.py
+++ TP 2/Utils.py
@@ -1,4 +1,5 @@
 import math
+from Matriz import Matriz
 
 # Función para calcular la cantidad de información
 # p: probabilidad del evento
@@ -36,3 +37,32 @@ def getAlfaProbabilidades(texto: str):
     for i in range(len(probabilidades)):
         probabilidades[i] /= longitud
     return alfabeto, probabilidades
+
+# Función que, a partir de una matriz de transición, devuelve la matriz de estados estables
+def estadosEstables(matriz: Matriz[float]) -> Matriz[float]:
+    if matriz.cantFilas != matriz.cantColumnas:
+        raise ValueError("La matriz debe ser cuadrada para calcular los estados estables.")
+    
+    n = matriz.cantFilas
+    A = [[0.0 for _ in range(n)] for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                A[i][j] = matriz.get(i, j) - 1.0
+            else:
+                A[i][j] = matriz.get(i, j)
+
+    for j in range(n):
+        A[n-1][j] = 1.0
+    
+    b = [0.0 for _ in range(n)]
+    b[n-1] = 1.0
+    
+    A_matriz = Matriz(n, n, A)
+    b_matriz = Matriz(n, 1, [[b[i]] for i in range(n)])
+    
+    A_inv = A_matriz.inversa
+    estados = A_inv * b_matriz
+    
+    return estados
