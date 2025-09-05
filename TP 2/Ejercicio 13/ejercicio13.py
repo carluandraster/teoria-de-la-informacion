18f14b9ea8f1a88aff8993bc331a8c8c3e8c8adb
carluandraster <andres.efstratiadis@gmail.com>
2025-09-04 22:56:02 -0300

    Update ejercicio13.py

--- TP 2/Ejercicio 13/ejercicio13.py
+++ TP 2/Ejercicio 13/ejercicio13.py
@@ -5,7 +5,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import Utils
 from Matriz import Matriz
 
+print("Inciso a)")
 M = Matriz(3,3, [[0.5, 1/3, 0],
                  [0.5, 1/3, 1],
                  [0,   1/3, 0]])
+print(M)
+
+print("Inciso b)")
 print(Utils.estadosEstables(M))
