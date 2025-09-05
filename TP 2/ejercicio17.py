f4438fdd0629eeac9158bbec9f59f0bceaa3e730
carluandraster <andres.efstratiadis@gmail.com>
2025-09-05 20:28:03 -0300

    Create ejercicio17.py

---
+++ TP 2/ejercicio17.py
@@ -0,0 +1,8 @@
+import ejercicio14 as ej14
+from Matriz import Matriz
+
+print("Grafo 2")
+matriz_de_transiciones = Matriz(4,4,[[0.5, 0, 0, 0.5],
+                                      [0.5, 0, 0, 0],
+                                      [0, 0.5, 0, 0],
+                                      [0, 0.5, 1, 0.5]])
