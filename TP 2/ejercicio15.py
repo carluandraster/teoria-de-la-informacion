54509c8e754c59cb9f22e1b924f52cbd37cd9946
carluandraster <andres.efstratiadis@gmail.com>
2025-09-05 15:16:56 -0300

    Create ejercicio15.py

---
+++ TP 2/ejercicio15.py
@@ -0,0 +1,27 @@
+from Matriz import Matriz
+
+# Dada una cadena de caracteres que representa un mensaje emitido por una fuente,
+# devuelve una lista con su alfabeto y su matriz de transiciónes de primer orden.
+def obtenerAlfabetoYTransiciones(mensaje: str) -> tuple[list[str], Matriz[float]]:
+    alfabeto = []
+    transiciones = []
+    longitud = len(mensaje)
+
+    # Obtener alfabeto
+    for simbolo in mensaje:
+        if simbolo not in alfabeto:
+            alfabeto.append(simbolo)
+
+    # Obtener matriz de transiciones
+    for i in range(longitud - 1):
+        fila = [0] * len(alfabeto)
+        fila[alfabeto.index(mensaje[i + 1])] += 1
+        transiciones.append(fila)
+
+    # Normalizar matriz de transiciones
+    for i in range(len(transiciones)):
+        total = sum(transiciones[i])
+        if total > 0:
+            transiciones[i] = [x / total for x in transiciones[i]]
+
+    return alfabeto, Matriz(len(transiciones), len(transiciones[0]), transiciones)
