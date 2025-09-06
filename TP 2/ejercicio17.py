ae4c794433288d66de1816e9a17f95c7c112ecaf
carluandraster <andres.efstratiadis@gmail.com>
2025-09-06 10:53:53 -0300

    Ejercicio 17

--- TP 2/ejercicio17.py
+++ TP 2/ejercicio17.py
@@ -8,4 +8,14 @@ matriz_de_transiciones = Matriz(4,4,[[0.5, 0, 0, 0.5],
                                       [0, 0.5, 1, 0.5]])
 print("Vector estacionario:")
 print(ej14.estadosEstables(matriz_de_transiciones))
-print("Entropía de la fuente: ", ej14.entropiaMarkoviana(matriz_de_transiciones), " bits")
+print("Entropía de la fuente: ", ej14.entropiaMarkoviana(matriz_de_transiciones), " bits\n")
+
+print("Grafo 3")
+matriz_de_transiciones = Matriz(5,5, [[1/3, 0, 1, 1/2, 0],
+                                      [1/3, 0, 0, 0, 0],
+                                      [0, 1, 0, 0, 0],
+                                      [1/3, 0, 0, 0, 1/2],
+                                      [0, 0, 0, 1/2, 1/2]])
+print("Vector estacionario:")
+print(ej14.estadosEstables(matriz_de_transiciones))
+print("Entropía de la fuente: ", ej14.entropiaMarkoviana(matriz_de_transiciones), " bits\n")
