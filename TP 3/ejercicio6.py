1a0399e38bd27d598c565344fa8d579d0fda58df
carluandraster <andres.efstratiadis@gmail.com>
2025-09-09 19:43:11 -0300

    Create ejercicio6.py

---
+++ TP 3/ejercicio6.py
@@ -0,0 +1,71 @@
+def esNoSingular(codigos: list) -> bool:
+    """
+    Determina si un codigo de encriptación es no singular; es decir, que no se repitan palabras codigo
+
+    Parametros:
+        - codigos (list): Lista de códigos.
+    Retorna:
+        - bool: True si el código es no singular, False en caso contrario.
+    Precondiciones:
+        - codigos no está vacía.
+        - codigos es distinto de None.
+    """
+    codigos_vistos = set()
+    for codigo in codigos:
+        if codigo in codigos_vistos:
+            return False
+        codigos_vistos.add(codigo)
+    return True
+
+def esInstantaneo(codigo: list) -> bool:
+    """
+    Verifica si un código es instantáneo.
+
+    Parámetros:
+        - codigo (list): Lista de cadenas que representan el código.
+    Retorna:
+        - bool: True si el código es instantáneo, False en caso contrario.
+    Precondiciones:
+        - codigo no está vacío.
+        - codigo es distinto de None
+    """
+    for i in range(len(codigo)):
+        for j in range(len(codigo)):
+            if i != j and codigo[i].startswith(codigo[j]):
+                return False
+    return True
+
+def esUnivocamenteDecodificable(codigo: list) -> bool:
+    """
+    Verifica si un código es unívocamente decodificable.
+
+    Parámetros:
+        - codigo (list): Lista de cadenas que representan el código.
+    Retorna:
+        - bool: True si el código es unívocamente decodificable, False en caso contrario.
+    Precondiciones:
+        - codigo no está vacío.
+        - codigo es distinto de None
+    """
+    S = [set(codigo), set()]
+    i = 0
+    seguir = True
+    while seguir:
+        for x in S[0]:
+            for y in S[i]:
+                if x.startswith(y) and x != y:
+                    S[i+1].add(x[len(y):])
+                else:
+                    if y.startswith(x) and x != y:
+                        S[i+1].add(y[len(x):])
+        if codigo.intersection(S[i+1]) != set(): # Si la intersección no es vacía, no es unívocamente decodificable
+            respuesta = False
+            seguir = False
+        else:
+            if S[i+1] == set() or S[i+1] in S[0:i+1]:
+                respuesta = True
+                seguir = False
+            else:
+                S.append(set())
+                i += 1
+    return respuesta
