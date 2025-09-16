f41ef39d2298d9b064715f1e33e8e6eba137e307
carluandraster <andres.efstratiadis@gmail.com>
2025-09-16 18:05:21 -0300

    Create ejercicio9.py

---
+++ TP 3/ejercicio9.py
@@ -0,0 +1,21 @@
+import math
+
+def get_cadena_caracteres(codigos: list[str])->str:
+    """
+    Dada una lista que contiene las palabras código de una codificación, obtiene una cadena de caracteres con el alfabeto código.
+
+    Parámetros:
+        - codigos: list[str] - Lista de palabras código.
+
+    Retorna:
+        - str - Cadena de caracteres con el alfabeto código.
+    
+    Contrato:
+        - Precondición: codigos debe ser una lista de cadenas no vacías y distina de None.
+        - Postcondición: El resultado es una cadena que contiene todos los caracteres únicos presentes en las palabras código.
+    """
+    n = math.floor(math.random()*25)
+    resultado = ""
+    for i in range(n):
+        resultado += codigos[math.floor(math.random()*len(codigos))]
+    return resultado
