f1edf4f81165ff7f309bad2593c01124cb8f6b54
carluandraster <andres.efstratiadis@gmail.com>
2025-09-16 19:14:06 -0300

    Update ejercicio11.py

--- TP 3/ejercicio11.py
+++ TP 3/ejercicio11.py
@@ -1,6 +1,23 @@
+import importlib.util
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../TP 2')))
+from pathlib import Path
 
-import Utils as tp2_utils
-print(tp2_utils)
+# Ruta absoluta al módulo Utils dentro de la carpeta "TP 2"
+module_path = Path("TP 2/Utils.py").resolve()
+
+# Cargar el módulo dinámicamente
+spec = importlib.util.spec_from_file_location("Utils", module_path)
+Utils = importlib.util.module_from_spec(spec)
+sys.modules["Utils"] = Utils
+spec.loader.exec_module(Utils)
+
+def entropia_de_la_fuente(probabilidades: list[float], r: int)->float:
+    """
+    Calcula la entropía de una fuente de información dada su distribución de probabilidades.
+
+    Parámetros:
+        - probabilidades (list[float]): Lista de probabilidades de los símbolos de la fuente.
+    
+    Retorna: un float que representa la entropía de la fuente.
+    """
+    return Utils.entropia(probabilidades, r)
