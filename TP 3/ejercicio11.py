ce4f6da7ea4a1222b1bee6c5f876b87fe3ce31a8
carluandraster <andres.efstratiadis@gmail.com>
2025-09-16 19:30:23 -0300

    Update ejercicio11.py

--- TP 3/ejercicio11.py
+++ TP 3/ejercicio11.py
@@ -1,3 +1,4 @@
+import ejercicio9 as ej9
 import importlib.util
 import sys
 from pathlib import Path
@@ -21,3 +22,24 @@ def entropia_de_la_fuente(probabilidades: list[float], r: int)->float:
     Retorna: un float que representa la entropía de la fuente.
     """
     return Utils.entropia(probabilidades, r)
+
+def get_longitud_media(palabras_codigo: list[str], probabilidades: list[float])->float:
+    """
+    Dada 2 listas, una de palabras código y otra de probabilidades, devuelve la longitud media del código.
+
+    Parámetros:
+        - palabras_codigo (list[str]): Lista de palabras código.
+        - probabilidades (list[float]): Lista de probabilidades de los símbolos de la fuente.
+    
+    Retorna: un float que representa la longitud media del código.
+
+    Contrato:
+        - len(palabras_codigo) == len(probabilidades)
+        - sum(probabilidades) == 1
+        - all(p >= 0 and p<=1 for p in probabilidades)
+    """
+    longitudes = ej9.get_longitudes(palabras_codigo)
+    L = 0
+    for p_i, l_i in zip(probabilidades, longitudes):
+        L += p_i * l_i
+    return L
