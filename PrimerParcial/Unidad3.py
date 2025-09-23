7be6342d9c3106eb2c2013f4b4a798bb28a15e13
carluandraster <andres.efstratiadis@gmail.com>
2025-09-23 18:39:52 -0300

    Update Unidad3.py

--- PrimerParcial/Unidad3.py
+++ PrimerParcial/Unidad3.py
@@ -1,4 +1,5 @@
 import math
+import random
 import Unidad2 as u2
 
 # Constantes
@@ -262,3 +263,27 @@ def es_codigo_compacto(codigos: list[str], probabilidades: list[float]) -> bool:
     else:
         respuesta = False
     return respuesta
+
+def simular_fuente(codigos: list[str], probabilidades: list[float], n: int) -> list[str]:
+    """
+    Simula la salida de una fuente de información discreta.
+
+    Parameteros:
+        - codigos: lista de símbolos del código.
+        - probabilidades: lista de probabilidades asociadas a cada símbolo.
+        - n: cantidad de símbolos a simular.
+    
+    Retorna: (list[str]) lista de símbolos simulados.
+
+    Contrato:
+        - len(codigos) > 0
+        - len(probabilidades) > 0
+        - len(codigos) == len(probabilidades)
+        - sum(probabilidades) == 1
+        - all(p >= 0 and p <= 1 for p in probabilidades)
+        - n > 0
+    """
+    simulacion = []
+    for _ in range(n):
+        simulacion.append(random.choices(codigos, weights=probabilidades)[0])
+    return simulacion
