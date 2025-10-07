5a4fdd186f266d455958ba157740b20fb5b3fb1a
carluandraster <andres.efstratiadis@gmail.com>
2025-10-07 18:38:08 -0300

    Pruebas

--- TP 4/ejercicio1.py
+++ TP 4/ejercicio1.py
@@ -12,6 +12,10 @@ def primer_teorema_shannon(probabilidades: list[float], palabras_codigo: list[st
     Retorna: true si se cumple el teorema, false en caso contrario.
     """
     s_n, probabilidades_n = generarConExtension(palabras_codigo, probabilidades, n)
+    print("Sn = ", s_n)	
+    print("Probabilidades n = ", probabilidades_n)
     longitud_media_n = get_longitud_media(s_n, probabilidades_n)
+    print("L = ", longitud_media_n)
     entropia_n = entropia_de_la_fuente(s_n, probabilidades_n)
+    print("H = ", entropia_n)
     return entropia_n <= longitud_media_n and longitud_media_n <= entropia_n + 1
