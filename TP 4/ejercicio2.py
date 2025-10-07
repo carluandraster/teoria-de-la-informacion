9a03d1daa57e066d657f2e679a9c8751dfcfb811
carluandraster <andres.efstratiadis@gmail.com>
2025-10-07 18:30:29 -0300

    Ejercicio 2

--- TP 4/ejercicio2.py
+++ TP 4/ejercicio2.py
@@ -4,6 +4,11 @@ probs = [0.3, 0.1, 0.4, 0.2]
 codigo = ["BA", "CAB", "A", "CBA"]
 N = 2
 
+if(ej1.primer_teorema_shannon(probs, codigo, 1)):
+    print("El código cumple con el primer teorema de Shannon.")
+else:
+    print("El código no cumple con el primer teorema de Shannon.")
+
 if(ej1.primer_teorema_shannon(probs, codigo, N)):
     print("La extensión de orden 2 del código cumple con el primer teorema de Shannon.")
 else:
