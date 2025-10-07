ace55efb4e0f425ca109e41972b5aff9d092f188
carluandraster <andres.efstratiadis@gmail.com>
2025-10-07 18:17:42 -0300

    Update ejercicio1.py

--- TP 4/ejercicio1.py
+++ TP 4/ejercicio1.py
@@ -1,3 +1,14 @@
-from Utils import generarConExtension
+from Utils import generarConExtension, entropia_de_la_fuente
 
 def primer_teorema_shannon(probabilidades: list[float], palabras_codigo: list[str], n: int) -> bool:
+    """
+    Verifica el primer teorema de Shannon para una fuente de información.
+
+    Parámetros:
+        probabilidades (list[float]): lista de probabilidades de cada símbolo en la fuente.
+        palabras_codigo (list[str]): lista de palabras del código asociado a la fuente.
+        n (int): longitud fija de las palabras del código.
+
+    Retorna: true si se cumple el teorema, false en caso contrario.
+    """
+    s_n, probabilidades_n = generarConExtension(palabras_codigo, probabilidades, n)
