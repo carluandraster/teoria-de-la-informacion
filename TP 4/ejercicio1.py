1d2edfce1d01fc213c304572d4584c029b337df0
carluandraster <andres.efstratiadis@gmail.com>
2025-10-07 18:24:42 -0300

    Update ejercicio1.py

--- TP 4/ejercicio1.py
+++ TP 4/ejercicio1.py
@@ -5,9 +5,9 @@ def primer_teorema_shannon(probabilidades: list[float], palabras_codigo: list[st
     Verifica el primer teorema de Shannon para una fuente de información.
 
     Parámetros:
-        probabilidades (list[float]): lista de probabilidades de cada símbolo en la fuente.
-        palabras_codigo (list[str]): lista de palabras del código asociado a la fuente.
-        n (int): longitud fija de las palabras del código.
+        - probabilidades (list[float]): lista de probabilidades de cada símbolo en la fuente.
+        - palabras_codigo (list[str]): lista de palabras del código asociado a la fuente.
+        - n (int): longitud fija de las palabras del código.
 
     Retorna: true si se cumple el teorema, false en caso contrario.
     """
