80682e07a372c252061ce121235b379d246cc867
carluandraster <andres.efstratiadis@gmail.com>
2025-10-07 18:24:09 -0300

    Update ejercicio1.py

--- TP 4/ejercicio1.py
+++ TP 4/ejercicio1.py
@@ -1,4 +1,4 @@
-from Utils import generarConExtension, entropia_de_la_fuente
+from Utils import generarConExtension, entropia_de_la_fuente, get_longitud_media
 
 def primer_teorema_shannon(probabilidades: list[float], palabras_codigo: list[str], n: int) -> bool:
     """
@@ -12,3 +12,6 @@ def primer_teorema_shannon(probabilidades: list[float], palabras_codigo: list[st
     Retorna: true si se cumple el teorema, false en caso contrario.
     """
     s_n, probabilidades_n = generarConExtension(palabras_codigo, probabilidades, n)
+    longitud_media_n = get_longitud_media(s_n, probabilidades_n)
+    entropia_n = entropia_de_la_fuente(s_n, probabilidades_n)
+    return entropia_n <= longitud_media_n and longitud_media_n <= entropia_n
