7bc0b999c317c1fd57fa59fcef60fe014ab109bc
carluandraster <andres.efstratiadis@gmail.com>
2025-10-10 17:57:53 -0300

    Update ejercicio17.py

--- TP 4/ejercicio17.py
+++ TP 4/ejercicio17.py
@@ -21,3 +21,34 @@ PROBABILIDADES = [
     0.031093, 0.033240, 0.008930, 0.000012, 0.000706,
     0.007851, 0.003199
 ]
+
+def generar_codificacion(probabilidades: list) -> tuple[list[str], float]:
+    """
+    Genera una codificación binaria óptima utilizando el algoritmo de Huffman o Shannon-Fano,
+    dependiendo del rendimiento.
+    
+    :param probabilidades: Lista de probabilidades de los símbolos.
+    :return: Una tupla que contiene la lista de códigos generados y el rendimiento.
+    """
+    codificacion_huffman = ej11.huffman(probabilidades)
+    rendimiento_huffman, _ = get_rendimiento_y_redundancia(probabilidades, codificacion_huffman)
+
+    codificacion_shannon_fano = ej11.shannon_fano(probabilidades)
+    rendimiento_shannon_fano, _ = get_rendimiento_y_redundancia(probabilidades, codificacion_shannon_fano)
+
+    if rendimiento_huffman >= rendimiento_shannon_fano:
+        return codificacion_huffman, rendimiento_huffman
+    else:
+        return codificacion_shannon_fano, rendimiento_shannon_fano
+
+if __name__ == "__main__":
+    codificacion, rendimiento = generar_codificacion(PROBABILIDADES)
+    print("Rendimiento: {:.4f}".format(rendimiento))
+    decodificador = Decodificador(SIMBOLOS, codificacion)
+    mensaje = input("Ingrese un mensaje: ")
+    mensaje_codificado = decodificador.codificar(mensaje)
+    
+    # Persistir en archivo binario
+    nombre_archivo = "mensaje_codificado.dat"
+    with open(nombre_archivo, 'wb') as archivo:
+        archivo.write(mensaje_codificado)
