5665fb59530ce3122cb09bf8a21ad0d51dc3e714
carluandraster <andres.efstratiadis@gmail.com>
2025-10-10 16:41:03 -0300

    Update ejercicio16.py

--- TP 4/ejercicio16.py
+++ TP 4/ejercicio16.py
@@ -7,8 +7,8 @@ def get_tasa_de_compresion(mensaje: str, mensaje_comprimido: bytearray) -> float
         mensaje_comprimido (bytearray): El mensaje comprimido.
 
     Returns:
-        float: La tasa de compresión, definida como la longitud del mensaje original
-               dividida por la longitud del mensaje comprimido.
+        float: La tasa de compresión, definida como la diferencia entre el tamaño del mensaje original
+               y el tamaño del mensaje comprimido, dividido por el tamaño del mensaje original.
     """
     tamanio_mensaje = len(mensaje)
     tamanio_mensaje_comprimido = len(mensaje_comprimido)
