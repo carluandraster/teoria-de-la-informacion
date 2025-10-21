ad631c6b6ff212ef4c4e20a18fa2c97721743422
carluandraster <andres.efstratiadis@gmail.com>
2025-10-21 19:16:45 -0300

    Update Canal.py

--- TP 5/Canal.py
+++ TP 5/Canal.py
@@ -1,7 +1,25 @@
 from ejercicio2 import get_matriz_del_canal
 from Utils import get_frecuencias
+from algebra_lineal.Matriz import Matriz
 
 class Canal:
+    """
+    Clase que representa un canal de comunicacion con una entrada y una salida.
+
+    Atributos:
+    ----------
+    - entrada : str
+        La cadena de caracteres que representa la entrada del canal.
+    - salida : str
+        La cadena de caracteres que representa la salida del canal.
+    
+    Metodos:
+    -------
+    - get_matriz_del_canal() -> Matriz[float]:
+        Devuelve la matriz de probabilidades condicionales del canal.
+    - get_probabilidades_a_priori() -> dict[str, float]:
+        Devuelve las probabilidades a priori de los simbolos en la entrada del canal.
+    """
     __entrada: str
     __salida: str
 
@@ -25,8 +43,16 @@ class Canal:
     def salida(self, salida: str):
         self.__salida = salida
 
-    def get_matriz_del_canal(self):
+    def get_matriz_del_canal(self) -> Matriz[float]:
+        """
+        Devuelve la matriz de probabilidades condicionales del canal.
+
+        :return: Matriz[float]
+        """
         return get_matriz_del_canal(self.__entrada, self.__salida)
     
-    def get_probabilidades_a_priori(self):
+    def get_probabilidades_a_priori(self) -> dict[str, float]:
+        """Devuelve las probabilidades a priori de los simbolos en la entrada del canal.
+        
+        :return: dict[str, float]"""
         return get_frecuencias(self.__entrada)
