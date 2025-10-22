2e1f1bf3a6a9fa3df52fbc1cc5a2e8bbc6bfd1ff
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 15:24:20 -0300

    Ejercicio 8

--- TP 5/ejercicio8.py
+++ TP 5/ejercicio8.py
@@ -7,14 +7,14 @@ from ejercicio7 import get_probabilidades_salidas
 def resolver(titulo: str, entrada: str, salida: str) -> None:
     CANAL = Canal(entrada, salida)
     print(titulo)
-    print("Probabilidades de los símbolos de salida: \n")
-    print("Sacando frecuencias relativas: ", get_frecuencias_relativas(salida))
-    probabilidades_a_priori = get_frecuencias_relativas(entrada).values()
+    print("Probabilidades de los símbolos de salida:")
+    print("Sacando frecuencias relativas: ", dict(sorted(get_frecuencias_relativas(salida).items())))
+    probabilidades_a_priori = dict(sorted(get_frecuencias_relativas(entrada).items())).values()
     matriz = CANAL.get_matriz_del_canal()
     print("Teniendo en cuenta las probabilidades a priori y la matriz del canal: ",
           get_probabilidades_salidas(probabilidades_a_priori, matriz))
 
 if __name__ == "__main__":
     resolver("Ejercicio 1", ENTRADA, SALIDA)
-    resolver("Ejercicio 3 - Canal 1", CANAL1.get_entrada(), CANAL1.get_salida())
-    resolver("Ejercicio 3 - Canal 2", CANAL2.get_entrada(), CANAL2.get_salida())
+    resolver("Ejercicio 3 - Canal 1", CANAL1.get_entrada, CANAL1.get_salida)
+    resolver("Ejercicio 3 - Canal 2", CANAL2.get_entrada, CANAL2.get_salida)
