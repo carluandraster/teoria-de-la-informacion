890e14b61d381573bfe00621bfde0a87d3ec7877
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 14:59:11 -0300

    Update Utils.py

--- TP 5/Utils.py
+++ TP 5/Utils.py
@@ -12,3 +12,15 @@ def get_frecuencias(texto: str)-> dict[str, int]:
         else:
             frecuencias[char] = 1
     return frecuencias
+
+def get_frecuencias_relativas(texto: str) -> dict[str, float]:
+    """
+    Esta función recibe un texto y devuelve un diccionario con la frecuencia relativa de cada carácter en el texto.
+    
+    :param texto: El texto del cual se quieren obtener las frecuencias relativas de los caracteres.
+    :return: Un diccionario donde las claves son los caracteres y los valores son sus frecuencias relativas.
+    """
+    frecuencias = get_frecuencias(texto)
+    total_caracteres = len(texto)
+    frecuencias_relativas = {char: freq / total_caracteres for char, freq in frecuencias.items()}
+    return frecuencias_relativas
