3b9d7d00e2d6cb759e1c8e58f20dfe56eafefd4e
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 14:57:11 -0300

    Update ejercicio1.py

--- TP 5/ejercicio1.py
+++ TP 5/ejercicio1.py
@@ -3,4 +3,5 @@ from ejercicio2 import get_matriz_del_canal
 ENTRADA = "abcacaabbcacaabcacaaabcaca"
 SALIDA =  "01010110011001000100010011"
 
-print(get_matriz_del_canal(ENTRADA, SALIDA))
+if __name__ == "__main__":
+    print(get_matriz_del_canal(ENTRADA, SALIDA))
