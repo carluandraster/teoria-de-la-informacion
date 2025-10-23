daadfcfe828ce268a2bf29ea02f1d578298b4204
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 21:48:53 -0300

    Update ejercicio6.py

--- TP 5/ejercicio6.py
+++ TP 5/ejercicio6.py
@@ -6,11 +6,12 @@ MATRIZ_CANAL = Matriz([[0.4, 0.4, 0.2],
                       [0.3, 0.2, 0.5],
                       [0.3, 0.4, 0.3]])
 
-print("a) Probabilidades de los simbolos de salida: ",
-      ej7.get_probabilidades_salidas(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
+if __name__ == "__main__":
+      print("a) Probabilidades de los simbolos de salida: ",
+            ej7.get_probabilidades_salidas(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
 
-print("b) Probabilidades a posteriori: \n",
-      ej7.get_probabilidades_a_posteriori(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
+      print("b) Probabilidades a posteriori: \n",
+            ej7.get_probabilidades_a_posteriori(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
 
-print("c) Probabilidades de eventos simultaneos: \n",
-      ej7.get_probabilidades_eventos_simultaneos(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
+      print("c) Probabilidades de eventos simultaneos: \n",
+            ej7.get_probabilidades_eventos_simultaneos(PROBABILIDADES_A_PRIORI, MATRIZ_CANAL))
