107bd8f2d564834063632a26d0a06941aeb89bc3
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 21:55:40 -0300

    Create ejercicio12.py

---
+++ TP 5/ejercicio12.py
@@ -0,0 +1,5 @@
+from ejercicio1 import ENTRADA, SALIDA
+from ejercicio3 import CANAL1, CANAL2
+from ejercicio6 import PROBABILIDADES_A_PRIORI, MATRIZ_CANAL
+from Utils import entropia, get_frecuencias_relativas
+from ejercicio11 import entropia_a_posteriori
