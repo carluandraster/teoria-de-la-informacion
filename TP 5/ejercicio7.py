05bed18a712dd2cdb785b7160d67969aa3e7c4e4
carluandraster <andres.efstratiadis@gmail.com>
2025-10-23 14:31:10 -0300

    Update ejercicio7.py

--- TP 5/ejercicio7.py
+++ TP 5/ejercicio7.py
@@ -21,10 +21,10 @@ def get_probabilidades_salidas(probabilidades_a_priori: list[float], matriz_cana
     :return: Lista de probabilidades de las salidas.
     """
     # Convertir la lista de probabilidades a priori en una matriz columna
-    matriz_entradas = Matriz([[p] for p in probabilidades_a_priori])
+    matriz_entradas = Matriz(probabilidades_a_priori)
     
     # Calcular la matriz de probabilidades de salidas
-    matriz_salidas = matriz_canal.traspuesta * matriz_entradas
+    matriz_salidas = matriz_entradas * matriz_canal
 
     # Devolver la lista de probabilidades de las salidas
     return [matriz_salidas[i][0] for i in range(matriz_salidas.cantidadFilas)]
