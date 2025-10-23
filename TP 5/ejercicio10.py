a5e710de8022b25927999e6269f39d2c35df2db9
carluandraster <andres.efstratiadis@gmail.com>
2025-10-22 21:45:13 -0300

    Create ejercicio10.py

---
+++ TP 5/ejercicio10.py
@@ -0,0 +1,17 @@
+from ejercicio11 import entropia_a_posteriori
+from Utils import entropia
+from algebra_lineal.Matriz import Matriz
+
+def resolver(titulo, probabilidades_a_priori, matriz_de_canal):
+    print(titulo)
+    print("Entropía a priori:", entropia(probabilidades_a_priori))
+    print("Entropía a posteriori:", entropia_a_posteriori(probabilidades_a_priori, matriz_de_canal))
+    print("-----------------------")
+
+if __name__ == "__main__":
+    resolver("Canal 1", [2/5, 3/5],
+             Matriz([[3/5, 2/5],
+                     [1/3, 2/3]]))
+    resolver("Canal 2", [3/4, 1/4],
+             Matriz([[2/3, 2/3],
+                     [1/10, 9/10]]))
