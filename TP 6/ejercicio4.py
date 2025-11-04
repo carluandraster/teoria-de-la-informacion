d05c46f6ea28f315ec7343e245c5094cbc2a46c5
carluandraster <andres.efstratiadis@gmail.com>
2025-11-04 19:21:54 -0300

    Update ejercicio4.py

--- TP 6/ejercicio4.py
+++ TP 6/ejercicio4.py
@@ -9,11 +9,13 @@ from algebra_lineal.Matriz import Matriz
 def get_matriz_compuesta(canal1: Matriz[float], canal2: Matriz[float]) -> Matriz[float]:
     """Calcula la matriz compuesta de dos canales de comunicación.
 
-    Args:
-        canal1 (Matriz): Matriz de transición del primer canal.
-        canal2 (Matriz): Matriz de transición del segundo canal.
+    Parámetros:
+    ---------------
+        - canal1 (Matriz): Matriz de transición del primer canal.
+        - canal2 (Matriz): Matriz de transición del segundo canal.
 
-    Returns:
+    Retorna:
+    -----------------
         Matriz: Matriz de transición del canal compuesto.
     """
     return canal1 * canal2	
