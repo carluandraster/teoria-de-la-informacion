566efcf02733acc8b5593148c78dc7685fdd26c1
carluandraster <andres.efstratiadis@gmail.com>
2025-11-04 10:28:57 -0300

    Update ejercicio14.py

--- TP 5/ejercicio14.py
+++ TP 5/ejercicio14.py
@@ -34,23 +34,14 @@ MAT_C4 = Matriz([
     [0.0, 0.0, 1.0]
 ])
 
-# def resolver(titulo: str, probs: list[float], matriz: Matriz[float]) -> None:
-#     print(titulo)
-#     print(f"H(A) = {entropia(probs):.4f} bits")
-#     probabilidades_salidas = get_probabilidades_salidas(probs, matriz)
-#     print(f"H(B) = {entropia(probabilidades_salidas):.4f} bits")
-#     print(f"P(b) = {probabilidades_salidas}")
-#     print(f"H(A/b) = {entropia_a_posteriori(probs, matriz)}")
-#     print(f"P(a, b) = {get_probabilidades_eventos_simultaneos(probs, matriz)}")
-#     print("-----------------\n")
-
 def resolver(titulo: str, probs: list[float], matriz: Matriz[float]) -> None:
     print(titulo)
+    print(f"H(A) = {entropia(probs):.4f} bits")
     probabilidades_salidas = get_probabilidades_salidas(probs, matriz)
-    print(f"a) H(A) = {entropia(probs):.4f} bits\t\tH(B) = {entropia(probabilidades_salidas):.4f} bits")
-    print(f"b) H(A/B) = {ej15.get_ruido(probs, matriz):.4f} bits\t\tH(B/A) = {ej15.get_perdida(probs, matriz):.4f} bits")
-    print(f"c) H(A,B) = {ej15.get_entropia_afin(probs, matriz):.4f} bits")
-    print(f"d) I(A,B) = I(B,A) = {ej15.get_informacion_mutua(probs, matriz):.4f} bits")
+    print(f"H(B) = {entropia(probabilidades_salidas):.4f} bits")
+    print(f"P(b) = {probabilidades_salidas}")
+    print(f"H(A/b) = {entropia_a_posteriori(probs, matriz)}")
+    print(f"P(a, b) = {get_probabilidades_eventos_simultaneos(probs, matriz)}")
     print("-----------------\n")
 
 resolver("C1", PROBS_C1, MAT_C1)
