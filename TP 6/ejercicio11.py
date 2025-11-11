c8b88dc567bd43c05940011e405d4e3735089d23
carluandraster <andres.efstratiadis@gmail.com>
2025-11-11 18:39:25 -0300

    Update ejercicio11.py

--- TP 6/ejercicio11.py
+++ TP 6/ejercicio11.py
@@ -1,3 +1,14 @@
 from ejercicio10 import maximizar_informacion_mutua
 from algebra_lineal.Matriz import Matriz
 
+CANAL_1 = Matriz([[0.60, 0.40],
+                  [0.20, 0.80]])
+
+CANAL_2 = Matriz([[0.25, 0.75],
+                  [0.90, 0.10]])
+
+CANAL_3 = Matriz([[0.51, 0.49],
+                  [0.72, 0.28]])
+
+CANAL_4 = Matriz([[0.77, 0.23],
+                  [0.20, 0.80]])
