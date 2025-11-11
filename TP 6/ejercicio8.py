71c07782f00e32edc0da0a2df3658aa2d156bbd3
carluandraster <andres.efstratiadis@gmail.com>
2025-11-11 18:25:01 -0300

    Probar ejercicio 9 en ejercicio 8

--- TP 6/ejercicio8.py
+++ TP 6/ejercicio8.py
@@ -6,4 +6,4 @@ CANALES = [CANAL_1, CANAL_2, CANAL_3, CANAL_4]
 if __name__ == "__main__":
     for i, canal in enumerate(CANALES, start=1):
         capacidad = capacidad_canal(canal)
-        print(f"Capacidad del CANAL_{i}: {capacidad:.4f} bits")
+        print(f"C_{i}: {capacidad:.4f} bits")
