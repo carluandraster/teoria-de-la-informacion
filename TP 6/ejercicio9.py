b43507d6a00a409bd82beac5b3ebd9122e57c3dd
carluandraster <andres.efstratiadis@gmail.com>
2025-11-11 18:20:38 -0300

    Update ejercicio9.py

--- TP 6/ejercicio9.py
+++ TP 6/ejercicio9.py
@@ -9,7 +9,10 @@ Funciones:
 - es_uniforme: Determina si un canal es uniforme.
 - capacidad_canal: Calcula la capacidad de un canal de comunicación."""
 
+from math import log2
 from algebra_lineal.Matriz import Matriz
+from Utils import entropia
+from ejercicio2 import es_canal_determinante, es_canal_sin_ruido
 
 def es_uniforme(matriz: Matriz) -> bool:
     """Determina si un canal es uniforme.
@@ -31,3 +34,29 @@ def es_uniforme(matriz: Matriz) -> bool:
         if matriz[i] not in primera_fila:
             return False
     return True
+
+def capacidad_canal(matriz: Matriz) -> float:
+    """
+    Determina la capacidad de un canal de comunicación.
+
+    La capacidad de un canal es la máxima cantidad de información que puede ser transmitida.
+
+    Parámetros:
+    -----------
+    matriz : Matriz
+        Matriz de transición del canal.
+
+    Retorna:
+    --------
+    float
+        Capacidad del canal.
+    """
+    if es_canal_determinante(matriz):
+        return log2(matriz.cantidadColumnas)
+    else:
+        if es_canal_sin_ruido(matriz):
+            return log2(matriz.cantidadFilas)
+        if es_uniforme(matriz):
+            return log2(matriz.cantidadColumnas) - entropia(matriz[0])
+        else:
+            raise ValueError("No se pudo clasificar el canal")
