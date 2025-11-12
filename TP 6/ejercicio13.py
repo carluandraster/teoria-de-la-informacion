f910b1c7a1cfe246389346ad11ff7088be045ff6
carluandraster <andres.efstratiadis@gmail.com>
2025-11-11 22:40:34 -0300

    Update ejercicio13.py

--- TP 6/ejercicio13.py
+++ TP 6/ejercicio13.py
@@ -1,4 +1,4 @@
-from Utils import get_probabilidades_salidas, get_probabilidades_a_posteriori
+from ejercicio14 import prob_error_canal
 from algebra_lineal.Matriz import Matriz
 
 CANAL = Matriz([[0.6, 0.3, 0.1],
@@ -12,9 +12,4 @@ PROBS_A_PRIORI_C = [4/15, 3/15, 8/15]
 if __name__ == "__main__":
     aprioris = [PROBS_A_PRIORI_A, PROBS_A_PRIORI_B, PROBS_A_PRIORI_C]
     for apriori in aprioris:
-        probs_salidas = get_probabilidades_salidas(apriori, CANAL)
-        probs_posteriori = get_probabilidades_a_posteriori(apriori, CANAL)
-        
-        print("P(b) = ", probs_salidas)
-        print("P(a|b)=\n", probs_posteriori.redondear(4))
-        print("\n")
+        print(f"P_E = {prob_error_canal(apriori, CANAL):.4f}")
