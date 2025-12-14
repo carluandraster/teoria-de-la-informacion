7dc33ba21991f476b1b619dd59817cdb172ff972
carluandraster <andres.efstratiadis@gmail.com>
2025-12-14 14:27:42 -0300

    Ejercicio 2

---
+++ TP 7/ejercicio2.py
@@ -0,0 +1,16 @@
+from CanalFactory import crear_bsc
+from Utils import capacidad_canal
+from segundo_teorema_shannon import get_M
+
+# Datos del ejercicio
+p = 0.11
+epsilon = 0.1
+n = 20
+canal = crear_bsc(p)
+
+if __name__ == "__main__":
+    C = capacidad_canal(canal.get_matriz_canal)
+    M = get_M(n, C, epsilon)
+    print(f"a) Capacidad del canal: {C:.4f} bits por uso")
+    print(f"b) R = {C-epsilon:.4f} bits por uso")
+    print(f"c) Número máximo de mensajes M para n={n} y ε={epsilon}: {M}")
